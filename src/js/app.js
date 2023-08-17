5edd0798e66e07bc44f9a52f4bd4945422fbfdf6
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2023-08-18 00:18:18 +0300

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import phoneNumberValidator from './basic';
+
+console.log('app worked');
+
+console.log(phoneNumberValidator('8 (927) 000-00-00'));
+console.log(phoneNumberValidator('+7 960 000 00 00'));
+console.log(phoneNumberValidator('+86 000 000 0000'));
